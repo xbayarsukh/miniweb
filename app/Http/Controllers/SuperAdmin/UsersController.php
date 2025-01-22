fa13161243da7254e18f74a993cb69c0bc79ce1f
xbayarsukh <90956420+xbayarsukh@users.noreply.github.com>
2025-01-22 11:34:39 +0800

    company name convert to subdomain

--- app/Http/Controllers/SuperAdmin/UsersController.php
+++ app/Http/Controllers/SuperAdmin/UsersController.php
@@ -56,4 +56,25 @@ class UsersController extends Controller
             return redirect()->route('admin.users')->with('success', 'Not found this portfolio maybe deleted');
         }
     }
+
+    public function createUser() {
+        $slug = preg_replace('/[^A-Za-z0-9\-]/', '', str_replace(' ', '', strtolower($this->transliterateMongolianToEnglish($request->name))));
+    }
+
+    function transliterateMongolianToEnglish($str) {
+        $mongolianMap = [
+            'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
+            'З' => 'Z', 'И' => 'I', 'Й' => 'I', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+            'Ө' => 'Ö', 'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ү' => 'Ü', 'Ф' => 'F',
+            'Х' => 'H', 'Ц' => 'Ts', 'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sch', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '',
+            'Э' => 'E', 'Ю' => 'Yu', 'Я' => 'Ya',
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+            'з' => 'z', 'и' => 'i', 'й' => 'i', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+            'ө' => 'ö', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ү' => 'ü', 'ф' => 'f',
+            'х' => 'h', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+        ];
+        
+        return strtr($str, $mongolianMap);
+    }
 }
